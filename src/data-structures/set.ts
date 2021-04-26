d937644b41e1dd7b7801b6ed653dc0d2c9c43fd3
Sergi Dote Teixidor <sergi.dote@kapsch.net>
2021-04-26 08:01:28 +0200

    changes

--- src/data-structures/set.ts
+++ src/data-structures/set.ts
@@ -1,21 +1,17 @@
-const union = <T>(setA: Set<T>, setB: Set<T>): Set<T> => new Set<T>([...setA, ...setB]);
+const union = ???; 
 
-const intersection = <T>(setA: Set<T>, setB: Set<T>): Set<T> => new Set<T>([...setA].filter(x => setB.has(x)));
+const intersection = ???; 
 
-const difference = <T>(setA: Set<T>, setB: Set<T>): Set<T> => new Set<T>([...setA].filter(x => !setB.has(x)));
+const difference = ???;
 
-const xor = <T>(setA: Set<T>, setB: Set<T>): Set<T> => new Set<T>([...setA].filter(x => !setB.has(x)).concat([...setB].filter(x => !setA.has(x))));
+const xor = ???;
 
-const subset = <T>(setA: Set<T>, setB: Set<T>): boolean => ([...setA].every(x => !setB.has(x)));
+const subset = ???;
 
 
 export function setTest() {
     const setA = new Set([1,2,3,4]);
     const setB = new Set([3,4,5,6]);
 
- console.log('union', union(setA, setB));
- console.log('intersection', intersection(setA, setB));
- console.log('difference', difference(setA, setB));
- console.log('A is subset of B?', subset(setA, setB));
- console.log('A is subset of B?', xor(setA, setB));
+ 
 }
